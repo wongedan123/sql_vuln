1227531a40ba6965cfef32be5743dae12acbef14
Wongedan <47300852+wongedan123@users.noreply.github.com>
2019-05-20 22:08:36 +0700

    Add files via upload
    How to install in termux/terminal linux<br><br>
    • apt-get install git -y<br>
    • git clone https://github.com/wongedan123/sql_vuln<br>
    • cd sql_vuln<br>
    • chmod +x module.sh<br>
    • ./module.sh<br>
    • perl sqlme.pl<br><br>
    
    Jika terdapat bug/error mohon hub: 08811664850

---
+++ module.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# sqlme module instalation
+clear
+echo "mohon tunggu installasi module membutuhkan waktu yang panjang mungkin kurang lebih 15mnt"
+sleep 2
+clear
+echo "STARTING INSTALLING MODULE PERL IN SYSTEM"
+apt-get update -y
+apt-get upgrade -y
+if [[ -d "/usr/bin/" ]]; then
+	if [[ $EUID -ne 0 ]]; then
+		echo "Your must be run as root"
+	else
+		apt-get install perl -y
+		cpan install LWP::UserAgent
+		cpan install Term::ANSIColor
+	fi
+
+elif [[ -d "/data/data/com.termux/" ]]; then
+		apt install perl -y
+		apt install -y autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk grep gzip libtool make patch perl sed silversearcher-ag tar wget
+		cpan install LWP::UserAgent
+		cpan install Term::ANSIColor
+else
+	exit
+fi
---
+++ sqlme.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+#Author : Ms.ambari
+#Date : 2018-12-23
+#Version: V.1.0
+
+use LWP::UserAgent;
+use Term::ANSIColor;
+my $ua = LWP::UserAgent->new;
+my %err = ("VULN" => "error in your SQL syntax",);
+my @result = ();
+$max = 5;
+sub main{
+	while(1){
+		print color("reset"),"[",color("green"),"*",color("reset"),"] Masukan_Dork: ";
+		$dork = <STDIN>; chomp($dork);
+		for (my $i = 1; $i <= $max; $i++) {
+			$target = "http://www1.search-results.com/web?q=".$dork."&tpr=1&ts=".$i;
+			$req = $ua->get($target);
+			$resp = $req->content;
+			while ($resp =~ m/<cite..class=\"algo-display[\D]*url\">(.*)<\/cite>/g) {
+				$hasil = $1."'";
+				push @result, $hasil;
+			}
+		}
+		my %has = map{$_ => 1} @result;
+		my @res = keys %has;
+		for (my $x = 0; $x < @res.length; $x++) {
+			$targets = @res[$x];
+			$reqs = $ua->get($targets);
+			$respcon = $reqs->content;
+			foreach $key(keys %err) {
+				$f = "'";
+				$rep = "";
+				$targets =~ s/$f/$rep/;
+				if ($respcon =~ %err{$key}) {
+					open(my $vulnweb,">>","sql_vuln.txt") || do ("$!");
+					say $vulnweb $targets;
+					print color("reset"),"[ ",color("green"),$key,color("reset")," ] ",color("reset"),$targets,"\n",color("reset");
+				}else{
+					print color("reset"),"[ ",color("red"),"NOT VULN",color("reset")," ] ",$targets,"\n",color("reset");
+				}
+			}
+		}
+	}
+}
+
+print color("green"),"              __          \n";
+print " .-----.-----|  |\n";
+print " |__ --|  _  |  |_\n";
+print " |_____|__   |____|",color("reset")," by : Mr.w0n63d4n\n",color("green");
+print "          |__|\n\n"; main();
